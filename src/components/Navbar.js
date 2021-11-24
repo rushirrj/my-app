dfc63f5897a92df544e934f61562215044c76753
Rushikesh Jadhav <80903066+rushirrj@users.noreply.github.com>
2021-11-24 16:59:03 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/my-app/">
+                <Link className="nav-link active" aria-current="page" to="my-app/">
                   Home
                 </Link>
               </li>
